e4dfca75080a70e3f172491dc54d75187f8c777e
Bekzod1999 <developmentfirm1999@gmail.com>
2022-08-16 11:20:12 +0500

    20

--- logic20.py
+++ logic20.py
@@ -8,5 +8,21 @@ def main(n):
     Returns:
         bool: answer
     """
+    #0 < n < 100000
+    #123456
+    a = n%10#6
+    n=n//10 #12345
 
-    return
+    b = n%10#5
+    n=n//10 #1234
+
+    c = n%10#4
+    n=n//10 #123
+
+    d = n%10#3
+    n=n//10 #12
+
+    e = n%10#2
+    f=n//10 #1
+    return a + b + c + d +f
+
