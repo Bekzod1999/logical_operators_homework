448a5800f46184396efa3cb6899a67f8dc9e8e64
Bekzod1999 <developmentfirm1999@gmail.com>
2022-08-18 00:32:20 +0500

    19

--- logic19.py
+++ logic19.py
@@ -1,3 +1,6 @@
+from re import X
+
+
 def main(x):
     """
     Given an integer x, return true if x is palindrome integer.
@@ -8,4 +11,10 @@ def main(x):
     Returns:
         bool: answer
     """
-    return
+    #9 < x < 1000
+
+    y = x%10 # 3
+    x = x//10 #32
+    z = x//10 #32%10 =2
+    #x=x//10 #32//10 =3
+    return (y == x or y == z) and y!=0
