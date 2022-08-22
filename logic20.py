88db7b9fa14450635d4665da7f05311ac92d3588
Bekzod1999 <developmentfirm1999@gmail.com>
2022-08-22 23:11:54 +0500

    20

--- logic20.py
+++ logic20.py
@@ -2,27 +2,32 @@ def main(n):
     """
     A number consisting of one and zero is given (the number starts at once). 
     If the number of ones is greater than zero, true, otherwise False is returned.
-    
+
     Args:
         n(int): parameter n
     Returns:
         bool: answer
     """
     #0 < n < 100000
-    #123456
-    a = n%10#6
-    n=n//10 #12345
+    # 101001
+    k = 6
+    a = n % 10  # 1
+    n = n//10  # 10100
 
-    b = n%10#5
-    n=n//10 #1234
+    b = n % 10  # 0
+    n = n//10  # 1010
 
-    c = n%10#4
-    n=n//10 #123
+    c = n % 10  # 4
+    n = n//10  # 123
 
-    d = n%10#3
-    n=n//10 #12
+    d = n % 10  # 3
+    n = n//10  # 12
 
-    e = n%10#2
-    f=n//10 #1
-    return a + b + c + d +f
-
+    e = n % 10  # 2
+    k += e
+    f = n//10  # 1
+    k += f
+    k -= (a + b + c + d + f)
+    return k > 3
+x=main(110)
+print(x)
